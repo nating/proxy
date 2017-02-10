9d10dec04baa6b648bd1df6a635743a05b0a378f
Geoffrey Natin <nating@tcd.ie>
2017-02-10 21:06:51 +0000

    Adding index, css, and some small caching code.

--- proxy.js
+++ proxy.js
@@ -1,5 +1,11 @@
 var http = require('http'),
-	url = require('url');
+	url = require('url'),
+	fs = require('fs');
+
+var config = JSON.parse(fs.readFileSync("config.json"));
+
+var host = config.host,
+	port = config.port;
 
 //	b == browser
 //	p == proxy
@@ -11,6 +17,7 @@ function notFound(res){
 }
 
 http.createServer(function(b_req,b_res){
+	console.log("Recieved a request for: "+b_req.url);
 
 	//Parse the requested URL
 	var b_url = url.parse(b_req.url,true);
@@ -21,41 +28,48 @@ http.createServer(function(b_req,b_res){
 	//Read and parse the query parameter
 	var p_url = url.parse(b_url.query.url);
 
-	//Initialize HTTP client
-	//var p_client = http.createClient(p_url.port || 80, p_url.hostname);
+	//Here is a bit of caching code
+	var file = fs.readFile("cache/"+p_url, function(error,data){
 
-	 //, request = http.createClient( port, host ) // .request( type, path, headers );
-	var options = {
-	    port     : p_url.port     || 80
-	,   host     : p_url.host     || 'localhost'
-	,   method   : p_url.type     || 'GET'
-	,   path     : p_url.path     || '/'
-	}
-	var p_req = http.request(options);
-	p_req.end();
+		//If not cached
+		if(error){
+			console.log("Not cached");
+			//Make http request
+			var options = {
+			    port     : p_url.port     || 80
+			,   host     : p_url.host     || 'localhost'
+			,   method   : p_url.type     || 'GET'
+			,   path     : p_url.path     || '/'
+			}
+			var p_req = http.request(options);
+			console.log("Made a request for:"+options.path);
+			p_req.end();
 
-	//Send request
-	//var p_req = p_client.request('GET',p_url.pathname || "/", { host: p_url.hostname });
-	//p_req.end();
+			//Handle responses
+			b_res.on('response', function(p_res){
+				console.log("1738");
+				//Pass headers through to browser
+				b_res.writeHead(p_res.statusCode, p_res.headers);
 
-	//Listen for response
-	p_req.addListener('response', function(p_res){
+				//Pass data through to browser
+				p_res.on('data',function(chunk){
+					b_res.write(chunk);
+				});
 
-		//Pass headers through to browser
-		b_res.writeHead(p_res.statusCode, p_res.headers);
-
-		//Pass data through to browser
-		p_res.addListener('data',function(chunk){
-			b_res.write(chunk);
-		});
-
-		p_res.addListener('end', function(){
-			b_res.end();
-		});
-
+				p_res.on('end', function(){
+					b_res.end();
+				});
+			});
+		}
+		//If cached
+		else{
+			console.log("Cached");
+			b_res.writeHead(200,{"Content-type":"text/html"});
+			b_res.end(data);
+		}
 	});
 
-}).listen(3000,'127.0.0.1');
 
-console.log("Server running at http://127.0.0.1:3000/");
-
+}).listen(port,host, function(){
+	console.log("Listening on:"+host+":"+port);
+});
